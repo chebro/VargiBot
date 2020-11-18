25e35427e6fd02850ba3358cfe6db706fd3899e8
Sravanth C <54331348+sravanth-chebrolu@users.noreply.github.com>
2020-11-18 13:55:50 +0530

    working version

---
+++ task_2/scripts/node_t2_ur5_1_pick_place.py
@@ -0,0 +1,251 @@
+#! /usr/bin/env python
+
+"""
+ROS Node - Pick and Place Module - MoveIt
+"""
+
+import sys
+import copy
+import math
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import actionlib
+
+from pkg_vb_sim.srv import vacuumGripper
+
+class Ur5Moveit:
+    """
+    Ur5Moveit Class Definition
+    """
+    def __init__(self):
+        rospy.init_node('node_eg4_go_to_pose', anonymous=True)
+
+        self._planning_group = "ur5_1_planning_group"
+        self._commander = moveit_commander.roscpp_initialize(sys.argv)
+        self._robot = moveit_commander.RobotCommander()
+        self._scene = moveit_commander.PlanningSceneInterface()
+        self._group = moveit_commander.MoveGroupCommander(self._planning_group)
+        self._display_trajectory_publisher = rospy.Publisher(
+            '/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=1)
+
+        self._exectute_trajectory_client = actionlib.SimpleActionClient(
+            'execute_trajectory', moveit_msgs.msg.ExecuteTrajectoryAction)
+        self._exectute_trajectory_client.wait_for_server()
+
+        self._planning_frame = self._group.get_planning_frame()
+        self._eef_link = self._group.get_end_effector_link()
+        self._group_names = self._robot.get_group_names()
+        self._box_name = ''
+
+        # Current State of the Robot is needed to add box to planning scene
+        self._curr_state = self._robot.get_current_state()
+
+        rospy.loginfo(
+            '\033[94m' + "Planning Group: {}".format(self._planning_frame) + '\033[0m')
+        rospy.loginfo(
+            '\033[94m' + "End Effector Link: {}".format(self._eef_link) + '\033[0m')
+        rospy.loginfo(
+            '\033[94m' + "Group Names: {}".format(self._group_names) + '\033[0m')
+
+        rospy.loginfo('\033[94m' + " >>> Ur5Moveit init done." + '\033[0m')
+
+    def wait_for_state_update(self, box_is_known=False, box_is_attached=False, timeout=4):
+        """
+        Ensuring Collision Updates Are Received
+        """
+        start = rospy.get_time()
+        seconds = rospy.get_time()
+        while (seconds - start < timeout) and not rospy.is_shutdown():
+            attached_objects = self._scene.get_attached_objects([self._box_name])
+            is_attached = len(attached_objects.keys()) > 0
+
+            is_known = self._box_name in self._scene.get_known_object_names()
+
+            if (box_is_attached == is_attached) and (box_is_known == is_known):
+                return True
+
+            rospy.sleep(0.1)
+            seconds = rospy.get_time()
+
+        return False
+
+
+    def add_box(self, timeout=4):
+        """
+        Adding Objects to the Planning Scene
+        """
+        box_pose = geometry_msgs.msg.PoseStamped()
+        box_pose.header.frame_id = "world"
+        box_pose.pose.orientation.w = 1.0
+        box_pose.pose.position.x = 0.04
+        box_pose.pose.position.y = 0.44
+        box_pose.pose.position.z = 1.87
+        self._box_name = "package$1"
+        self._scene.add_box(self._box_name, box_pose, size=(0.15, 0.15, 0.15))
+
+        return self.wait_for_state_update(box_is_known=True, timeout=timeout)
+
+
+    def attach_box(self, timeout=4):
+        """
+        Attaching Objects to the Robot
+        """
+        grasping_group = "ur5_1_planning_group"
+        touch_links = self._robot.get_link_names(group=grasping_group)
+        self._scene.attach_box(self._eef_link, self._box_name, touch_links=touch_links)
+
+        return self.wait_for_state_update(box_is_attached=True, box_is_known=False, timeout=timeout)
+
+
+    def detach_box(self, timeout=4):
+        """
+        Detaching Objects from the Robot
+        """
+        self._scene.remove_attached_object(self._eef_link, name=self._box_name)
+
+        return self.wait_for_state_update(box_is_known=True, box_is_attached=False, timeout=timeout)
+
+
+    def remove_box(self, timeout=4):
+        """
+        Removing Objects from the Planning Scene
+        """
+        self._scene.remove_world_object(self._box_name)
+
+        return self.wait_for_state_update(box_is_attached=False,
+                                          box_is_known=False,
+                                          timeout=timeout)
+
+    def go_to_pose(self, arg_pose):
+        """
+        Plan and Execute : Go to Pose
+        """
+        pose_values = self._group.get_current_pose().pose
+        rospy.loginfo('\033[94m' + ">>> Current Pose:" + '\033[0m')
+        rospy.loginfo(pose_values)
+
+        self._group.set_pose_target(arg_pose)
+        flag_plan = self._group.go(wait=True)
+
+        pose_values = self._group.get_current_pose().pose
+        rospy.loginfo('\033[94m' + ">>> Final Pose:" + '\033[0m')
+        rospy.loginfo(pose_values)
+
+        list_joint_values = self._group.get_current_joint_values()
+        rospy.loginfo('\033[94m' + ">>> Final Joint Values:" + '\033[0m')
+        rospy.loginfo(list_joint_values)
+
+        if flag_plan:
+            rospy.loginfo(
+                '\033[94m' + ">>> go_to_pose() Success" + '\033[0m')
+        else:
+            rospy.logerr(
+                '\033[94m' + ">>> go_to_pose() Failed. Solution for Pose not Found." + '\033[0m')
+
+        return flag_plan
+
+    def set_joint_angles(self, arg_list_joint_angles):
+        """
+        Plan and Execute : Set Joint Angles
+        """
+        list_joint_values = self._group.get_current_joint_values()
+        rospy.loginfo('\033[94m' + ">>> Current Joint Values:" + '\033[0m')
+        rospy.loginfo(list_joint_values)
+
+        self._group.set_joint_value_target(arg_list_joint_angles)
+        self._group.plan()
+        flag_plan = self._group.go(wait=True)
+
+        list_joint_values = self._group.get_current_joint_values()
+        rospy.loginfo('\033[94m' + ">>> Final Joint Values:" + '\033[0m')
+        rospy.loginfo(list_joint_values)
+
+        pose_values = self._group.get_current_pose().pose
+        rospy.loginfo('\033[94m' + ">>> Final Pose:" + '\033[0m')
+        rospy.loginfo(pose_values)
+
+        if flag_plan:
+            rospy.loginfo(
+                '\033[94m' + ">>> set_joint_angles() Success" + '\033[0m')
+        else:
+            rospy.logerr(
+                '\033[94m' + ">>> set_joint_angles() Failed." + '\033[0m')
+
+        return flag_plan
+
+    def go_to_predefined_pose(self, arg_pose_name):
+        """
+        Plan and Execute : Pre-defined Pose
+        """
+        rospy.loginfo('\033[94m' + "Going to Pose: {}".format(arg_pose_name) + '\033[0m')
+        self._group.set_named_target(arg_pose_name)
+        plan = self._group.plan()
+        goal = moveit_msgs.msg.ExecuteTrajectoryGoal()
+        goal.trajectory = plan
+        self._exectute_trajectory_client.send_goal(goal)
+        self._exectute_trajectory_client.wait_for_result()
+        rospy.loginfo('\033[94m' + "Now at Pose: {}".format(arg_pose_name) + '\033[0m')
+
+    def activate_vacuum_gripper(self, state):
+        """
+        Enable/Disable Gripper Module
+        """
+        rospy.wait_for_service('/eyrc/vb/ur5_1/activate_vacuum_gripper')
+        try:
+            activate_vacuum_gripper = rospy.ServiceProxy('/eyrc/vb/ur5_1/activate_vacuum_gripper', vacuumGripper)
+            resp1 = activate_vacuum_gripper(state)
+            return resp1
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
+
+    # Destructor
+    def __del__(self):
+        """
+        """
+        moveit_commander.roscpp_shutdown()
+        rospy.loginfo(
+            '\033[94m' + "Object of class Ur5Moveit Deleted." + '\033[0m')
+
+
+def main():
+    """
+    Main Function
+    """
+    ur5 = Ur5Moveit()
+
+    lst_joint_angles_0 = [math.radians(-69),
+                          math.radians(-107),
+                          math.radians(3),
+                          math.radians(-76),
+                          math.radians(69),
+                          math.radians(180)]
+
+    lst_joint_angles_1 = [math.radians(-180),
+                          math.radians(0),
+                          math.radians(0),
+                          math.radians(-90),
+                          math.radians(-90),
+                          math.radians(0)]
+
+    ur5.add_box()
+    ur5.set_joint_angles(lst_joint_angles_0)
+    rospy.sleep(2)
+    ur5.activate_vacuum_gripper(True)
+    ur5.attach_box()
+    ur5.set_joint_angles(lst_joint_angles_1)
+    rospy.sleep(2)
+    ur5.activate_vacuum_gripper(False)
+    ur5.detach_box()
+    ur5.remove_box()
+    ur5.go_to_predefined_pose("allZeros")
+    rospy.sleep(2)
+    rospy.sleep(1000)
+
+    del ur5
+
+
+if __name__ == '__main__':
+    main()
+
